c322d8dc3a6ad839fe6547ce3d476a5fe1274ed6
Rodrigo Serradura <rserradura@gmail.com>
2011-06-16 21:03:30 -0700

    refatorado os teste da lib tag

--- test/string_utility_belt/tags_test.rb
+++ test/string_utility_belt/tags_test.rb
@@ -6,15 +6,15 @@ class TagsTest < Test::Unit::TestCase
     assert_equal "", "<html>".html_tag_cleaner
   end
 
-  def test_should_not_cleaner_if_is_not_a_html_tag
+  def test_should_not_clean_up_if_is_not_a_html_tag
     assert_equal "<serradura>", "<serradura>".html_tag_cleaner
   end
 
-  def test_should_cleaner_all_tags_and_just_keep_the_text
+  def test_should_clean_up_all_tags_and_just_keep_the_text
     assert_equal "Serradura Labs", "<a href='http://blog.serraduralabs.com'>Serradura Labs</a>".html_tag_cleaner
   end
 
-  def test_should_cleaner_anything_like_an_tag
+  def test_should_clean_up_anything_resembling_a_tag
     assert_equal "", "<a></b></serradura><></>".tag_cleaner
   end
 
