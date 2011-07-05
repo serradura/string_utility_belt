0b9f0f3f013ad41361386b6577c05be8bc551497
Rodrigo Serradura <rserradura@gmail.com>
2011-07-05 17:47:07 -0300

    Adicionado novos testes ao regex_me_to_search

--- test/string_utility_belt/regex_me_to_search_test.rb
+++ test/string_utility_belt/regex_me_to_search_test.rb
@@ -6,4 +6,14 @@ class RegexMeToSearchTest < Test::Unit::TestCase
     assert_equal(/(foo|bar)/, "foo bar".regex_me_to_search_ruby)
   end
 
+  def test_should_return_a_regex_in_mysql
+    assert_equal("(foo|bar)", "foo bar".regex_me_to_search_mysql)
+  end
+
+  def test_describe_exact_phrases_when_want_match_two_terms_in_ruby
+    text  = "ruby frameworks is so cool"
+    regexp = "ruby frameworks".regex_me_to_search_ruby(:exact_phrase => true)
+    assert_match(regexp, text)
+  end
+
 end
