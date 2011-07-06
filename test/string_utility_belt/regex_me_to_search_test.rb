307ce49ff714ac5dd99c4e4f901f3055968f8fce
Rodrigo Serradura <rserradura@gmail.com>
2011-07-05 20:53:32 -0700

    testes aprimorados

--- test/string_utility_belt/regex_me_to_search_test.rb
+++ test/string_utility_belt/regex_me_to_search_test.rb
@@ -2,6 +2,10 @@ require File.join(File.dirname(__FILE__), '..', 'test_helper')
 
 class RegexMeToSearchTest < Test::Unit::TestCase
 
+  def setup
+    @text = "ruby frameworks is so cool"
+  end
+
   def test_should_return_a_regex_for_ruby
     assert_equal(/(foo|bar)/, "foo bar".regex_me_to_search_ruby)
   end
@@ -11,14 +15,52 @@ class RegexMeToSearchTest < Test::Unit::TestCase
   end
 
   def test_should_match_the_phrase_for_ruby
-    text  = "ruby frameworks is so cool"
     regexp = "ruby frameworks".regex_me_to_search_ruby(:exact_phrase => true)
-    assert_match(regexp, text)
+    assert_match(regexp, @text)
   end
 
   def test_should_match_independent_of_the_non_word_chars_between_the_words_phrase_for_ruby
     text  = "ruby - on - rails"
     regexp = "ruby on rails".regex_me_to_search_ruby(:exact_phrase => true)
+    assert_match(regexp, text)
+  end
+
+  def test_should_match_the_text_fragments
+    regexp = "rub ool".regex_me_to_search_ruby(:exact_word => false)
+    assert_match(regexp, @text)
+  end
+
+  def test_should_not_match_the_text_fragments
+    regexp = "rub ool".regex_me_to_search_ruby(:exact_word => true)
+    assert_no_match(regexp, @text)
+  end
+
+  def test_should_just_match_exact_word
+    regexp = "ruby cool".regex_me_to_search_ruby(:exact_word => true)
+    assert_match(regexp, @text)
+  end
+
+  def test_should_match_because_the_word_finish_with_the_letter_A
+    text  = "sinatra"
+    regexp = "*a".regex_me_to_search_ruby
+    assert_match(regexp, text)
+  end
+
+  def test_should_match_because_the_word_begin_with_the_letter_A
+    text  = "assertions"
+    regexp = "a*".regex_me_to_search_ruby
+    assert_match(regexp, text)
+  end
+
+  def test_should_match_because_the_word_contain_all_required_letters
+    text  = "sinatraaa"
+    regexp = "s*n*t*a".regex_me_to_search_ruby
+    assert_match(regexp, text)
+  end
+
+  def test_should_return_the_matched_sentencest
+    text  = "sinatra"
+    regexp = "*a".regex_me_to_search_ruby
     assert_match(regexp, text)
   end
 
